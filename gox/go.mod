a2886773cbe37d8f7d8820a645dc05e70d27d9a3
sky91 <feitiandaxia1991@163.com>
2024-02-06 17:33:24 +0800

    feat: must

---
+++ gox/go.mod
@@ -0,0 +1,3 @@
+module github.com/sky91/letsgo/gox
+
+go 1.21
---
+++ gox/panicx/panic.go
@@ -0,0 +1,12 @@
+package panicx
+
+func Must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Must1[T any](t T, err error) T {
+	Must(err)
+	return t
+}
