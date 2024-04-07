a4bca9a0b06f9d1bf598cf1e8525ee08b2a8b4b9
sky91 <feitiandaxia1991@gmail.com>
2024-04-07 21:58:38 +0800

    feat: shutdown

---
+++ gox/lifecycle/shutdown.go
@@ -0,0 +1,65 @@
+package lifecycle
+
+import "context"
+
+type FnShutdown[T any] struct {
+	Value      T
+	ShutdownFn func()
+}
+
+func (thisP *FnShutdown[T]) Shutdown() { thisP.ShutdownFn() }
+
+type ErrFnShutdown[T any] struct {
+	Value      T
+	ShutdownFn func() error
+}
+
+func (thisP *ErrFnShutdown[T]) Shutdown() error { return thisP.ShutdownFn() }
+
+type CtxFnShutdown[T any] struct {
+	Value      T
+	ShutdownFn func(context.Context)
+}
+
+func (thisP *CtxFnShutdown[T]) Shutdown(ctx context.Context) { thisP.ShutdownFn(ctx) }
+
+type CtxErrFnShutdown[T any] struct {
+	Value      T
+	ShutdownFn func(context.Context) error
+}
+
+func (thisP *CtxErrFnShutdown[T]) Shutdown(ctx context.Context) error {
+	return thisP.ShutdownFn(ctx)
+}
+
+type CloserShutdown[T interface{ Close() }] struct {
+	Value T
+}
+
+func (thisP *CloserShutdown[T]) Shutdown() {
+	thisP.Value.Close()
+}
+
+type ErrCloserShutdown[T interface{ Close() error }] struct {
+	Value T
+}
+
+func (thisP *ErrCloserShutdown[T]) Shutdown() error {
+	return thisP.Value.Close()
+}
+
+type CtxCloserShutdown[T interface{ Close(context.Context) }] struct {
+	Value T
+}
+
+func (thisP *CtxCloserShutdown[T]) Shutdown(ctx context.Context) {
+	thisP.Value.Close(ctx)
+}
+
+type CtxErrCloserShutdown[T interface{ Close(context.Context) error }] struct {
+	Value T
+}
+
+func (thisP *CtxErrCloserShutdown[T]) Shutdown(ctx context.Context) error {
+	return thisP.Value.Close(ctx)
+}
