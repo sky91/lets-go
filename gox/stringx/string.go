09023fbfaf51262eddfad18f65ce2bd684cd486f
sky91 <feitiandaxia1991@gmail.com>
2024-04-20 11:52:55 +0800

    feat: Nil2Zero

--- gox/stringx/string.go
+++ gox/stringx/string.go
@@ -1,5 +1,11 @@
 package stringx
 
+import "github.com/sky91/lets-go/gox"
+
 func NewString(s string) *string {
 	return &s
+}
+
+func Nil2Empty(s *string) string {
+	return gox.Nil2Zero(s)
 }
--- gox/value.go
+++ gox/value.go
@@ -4,3 +4,10 @@ type Value[T any] interface {
 	Val() T
 	Ptr() *T
 }
+
+func Nil2Zero[T any](t *T) (r T) {
+	if t == nil {
+		return
+	}
+	return *t
+}
