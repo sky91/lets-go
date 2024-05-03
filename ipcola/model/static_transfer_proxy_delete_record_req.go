84dc23dccd4b6d5c69e7b7039ff20a51834e6a25
sky91 <feitiandaxia1991@gmail.com>
2024-05-03 18:28:02 +0800

    feat: ipcola

---
+++ ipcola/model/static_transfer_proxy_delete_record_req.go
@@ -0,0 +1,5 @@
+package model
+
+type StaticTransferProxyDeleteRecordReq struct {
+	ID int64 `json:"id"`
+}
---
+++ ipcola/model/static_transfer_proxy_delete_record_resp.go
@@ -0,0 +1,6 @@
+package model
+
+type StaticTransferProxyDeleteRecordResp struct {
+	MetaMessage string `json:"meta_message"`
+	MetaStatus  int64  `json:"meta_status"`
+}
