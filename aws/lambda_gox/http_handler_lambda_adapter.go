aab6624792e1513db97df88174c78da67c3c08f6
sky91 <feitiandaxia1991@gmail.com>
2024-07-19 19:50:02 +0800

    feat: HandleFunctionUrl

--- aws/lambda_gox/http_handler_lambda_adapter.go
+++ aws/lambda_gox/http_handler_lambda_adapter.go
@@ -33,7 +33,7 @@ type HttpHandlerToLambdaAdapter struct {
 }
 
 func (thisV HttpHandlerToLambdaAdapter) HandleHttpApi(ctx context.Context, request *events.APIGatewayV2HTTPRequest) (*events.APIGatewayV2HTTPResponse, error) {
-	var body io.Reader = strings.NewReader(request.Body)
+	body := io.Reader(strings.NewReader(request.Body))
 	if request.IsBase64Encoded {
 		body = base64.NewDecoder(base64.StdEncoding, body)
 	}
@@ -57,6 +57,48 @@ func (thisV HttpHandlerToLambdaAdapter) HandleHttpApi(ctx context.Context, reque
 	thisV.HttpHandler.ServeHTTP(&respWriter, httpRequest)
 
 	resp := &events.APIGatewayV2HTTPResponse{
+		StatusCode:      respWriter.statusCode,
+		Body:            base64.StdEncoding.EncodeToString(respWriter.body.Bytes()),
+		IsBase64Encoded: true}
+
+	if hdrCnt := len(respWriter.header); hdrCnt > 0 {
+		resp.Headers = make(map[string]string, hdrCnt)
+		for k, v := range respWriter.header {
+			if k == "Set-Cookie" {
+				resp.Cookies = v
+			} else {
+				resp.Headers[k] = strings.Join(v, ",")
+			}
+		}
+	}
+	return resp, nil
+}
+
+func (thisV HttpHandlerToLambdaAdapter) HandleFunctionUrl(ctx context.Context, request *events.LambdaFunctionURLRequest) (*events.LambdaFunctionURLResponse, error) {
+	body := io.Reader(strings.NewReader(request.Body))
+	if request.IsBase64Encoded {
+		body = base64.NewDecoder(base64.StdEncoding, body)
+	}
+	url := "https://" + request.RequestContext.DomainName + request.RawPath
+	if request.RawQueryString != "" {
+		url += "?" + request.RawQueryString
+	}
+	ctx = context.WithValue(ctx, ReqCtxKey{}, request)
+	httpRequest, err := http.NewRequestWithContext(ctx, request.RequestContext.HTTP.Method, url, body)
+	if err != nil {
+		return nil, err
+	}
+	httpRequest.RemoteAddr = request.RequestContext.HTTP.SourceIP
+	for k, v := range request.Headers {
+		httpRequest.Header.Add(k, v)
+	}
+
+	respWriter := bufferedHttpRespWriter{header: make(http.Header, 32)}
+	respWriter.body.Grow(8192)
+
+	thisV.HttpHandler.ServeHTTP(&respWriter, httpRequest)
+
+	resp := &events.LambdaFunctionURLResponse{
 		StatusCode:      respWriter.statusCode,
 		Body:            base64.StdEncoding.EncodeToString(respWriter.body.Bytes()),
 		IsBase64Encoded: true}
