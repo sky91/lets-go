63097106c6b5462052f7c5649e9243d4bffbeb8b
sky91 <feitiandaxia1991@gmail.com>
2024-07-27 01:06:35 +0800

    feat: dox

--- dox/dox.go
+++ dox/dox.go
@@ -25,3 +25,27 @@ func LazyTransferNamed[Dep, T any](name string, transfer func(dep Dep) (T, error
 		})
 	}
 }
+
+func LazyTransferStruct[Dep, T any](transfer func(dep *Dep) (T, error)) func(do.Injector) {
+	return func(injector do.Injector) {
+		do.Provide(injector, func(injector do.Injector) (t T, err error) {
+			dep, err := do.InvokeStruct[Dep](injector)
+			if err != nil {
+				return t, err
+			}
+			return transfer(dep)
+		})
+	}
+}
+
+func LazyTransferStructNamed[Dep, T any](name string, transfer func(dep *Dep) (T, error)) func(do.Injector) {
+	return func(injector do.Injector) {
+		do.ProvideNamed(injector, name, func(injector do.Injector) (t T, err error) {
+			dep, err := do.InvokeStruct[Dep](injector)
+			if err != nil {
+				return t, err
+			}
+			return transfer(dep)
+		})
+	}
+}
