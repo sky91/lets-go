20d62af8ee5ac4decc778aedd2066c6a4b332e75
sky91 <feitiandaxia1991@gmail.com>
2024-10-09 22:43:02 +0800

    feat: HttpHandlerToLambdaAdapter

--- aws/lambda_gox/http_handler_lambda_adapter.go
+++ aws/lambda_gox/http_handler_lambda_adapter.go
@@ -4,9 +4,11 @@ import (
 	"bytes"
 	"context"
 	"encoding/base64"
+	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"io"
 	"net/http"
+	"net/url"
 	"strings"
 )
 
@@ -32,88 +34,120 @@ type HttpHandlerToLambdaAdapter struct {
 	HttpHandler http.Handler
 }
 
-func (thisV HttpHandlerToLambdaAdapter) HandleHttpApi(ctx context.Context, request *events.APIGatewayV2HTTPRequest) (*events.APIGatewayV2HTTPResponse, error) {
+func (thisV HttpHandlerToLambdaAdapter) HandleHttpApi(ctx context.Context, request *events.APIGatewayV2HTTPRequest) (response *events.APIGatewayV2HTTPResponse, err error) {
+	httpReq := commonHttpReq.WithContext(context.WithValue(ctx, ReqCtxKey{}, request))
+	httpReq.Method = request.RequestContext.HTTP.Method
+	httpReq.Proto = request.RequestContext.HTTP.Protocol
+
+	var ok bool
+	if httpReq.ProtoMajor, httpReq.ProtoMinor, ok = http.ParseHTTPVersion(httpReq.Proto); !ok {
+		return nil, fmt.Errorf("malformed http proto: [%s]", httpReq.Proto)
+	}
+
+	httpReq.RequestURI = request.RawPath
+	if request.RawQueryString != "" {
+		httpReq.RequestURI += "?" + request.RawQueryString
+	}
+	if httpReq.URL, err = url.ParseRequestURI(httpReq.RequestURI); err != nil {
+		return nil, err
+	}
+
+	httpReq.Header = make(http.Header, len(request.Headers))
+	for k, v := range request.Headers {
+		httpReq.Header.Add(k, v)
+	}
+	httpReq.Host = httpReq.Header.Get("Host")
+
 	body := io.Reader(strings.NewReader(request.Body))
 	if request.IsBase64Encoded {
 		body = base64.NewDecoder(base64.StdEncoding, body)
+		httpReq.ContentLength = int64(base64.StdEncoding.DecodedLen(len(request.Body)))
+	} else {
+		httpReq.ContentLength = int64(len(request.Body))
 	}
-	url := "https://" + request.RequestContext.DomainName + request.RawPath
-	if request.RawQueryString != "" {
-		url += "?" + request.RawQueryString
-	}
-	ctx = context.WithValue(ctx, ReqCtxKey{}, request)
-	httpRequest, err := http.NewRequestWithContext(ctx, request.RequestContext.HTTP.Method, url, body)
-	if err != nil {
-		return nil, err
-	}
-	httpRequest.RemoteAddr = request.RequestContext.HTTP.SourceIP
-	for k, v := range request.Headers {
-		httpRequest.Header.Add(k, v)
-	}
+	httpReq.Body = io.NopCloser(body)
 
 	respWriter := bufferedHttpRespWriter{header: make(http.Header, 32)}
 	respWriter.body.Grow(8192)
 
-	thisV.HttpHandler.ServeHTTP(&respWriter, httpRequest)
+	thisV.HttpHandler.ServeHTTP(&respWriter, httpReq)
 
-	resp := &events.APIGatewayV2HTTPResponse{
+	response = &events.APIGatewayV2HTTPResponse{
 		StatusCode:      respWriter.statusCode,
 		Body:            base64.StdEncoding.EncodeToString(respWriter.body.Bytes()),
-		IsBase64Encoded: true}
+		IsBase64Encoded: true,
+	}
 
 	if hdrCnt := len(respWriter.header); hdrCnt > 0 {
-		resp.Headers = make(map[string]string, hdrCnt)
+		response.Headers = make(map[string]string, hdrCnt)
 		for k, v := range respWriter.header {
 			if k == "Set-Cookie" {
-				resp.Cookies = v
+				response.Cookies = v
 			} else {
-				resp.Headers[k] = strings.Join(v, ",")
+				response.Headers[k] = strings.Join(v, ",")
 			}
 		}
 	}
-	return resp, nil
+	return response, nil
 }
 
-func (thisV HttpHandlerToLambdaAdapter) HandleFunctionUrl(ctx context.Context, request *events.LambdaFunctionURLRequest) (*events.LambdaFunctionURLResponse, error) {
+func (thisV HttpHandlerToLambdaAdapter) HandleFunctionUrl(ctx context.Context, request *events.LambdaFunctionURLRequest) (response *events.LambdaFunctionURLResponse, err error) {
+	httpReq := commonHttpReq.WithContext(context.WithValue(ctx, ReqCtxKey{}, request))
+	httpReq.Method = request.RequestContext.HTTP.Method
+	httpReq.Proto = request.RequestContext.HTTP.Protocol
+
+	var ok bool
+	if httpReq.ProtoMajor, httpReq.ProtoMinor, ok = http.ParseHTTPVersion(httpReq.Proto); !ok {
+		return nil, fmt.Errorf("malformed http proto: [%s]", httpReq.Proto)
+	}
+
+	httpReq.RequestURI = request.RawPath
+	if request.RawQueryString != "" {
+		httpReq.RequestURI += "?" + request.RawQueryString
+	}
+	if httpReq.URL, err = url.ParseRequestURI(httpReq.RequestURI); err != nil {
+		return nil, err
+	}
+
+	httpReq.Header = make(http.Header, len(request.Headers))
+	for k, v := range request.Headers {
+		httpReq.Header.Add(k, v)
+	}
+	httpReq.Host = httpReq.Header.Get("Host")
+
 	body := io.Reader(strings.NewReader(request.Body))
 	if request.IsBase64Encoded {
 		body = base64.NewDecoder(base64.StdEncoding, body)
+		httpReq.ContentLength = int64(base64.StdEncoding.DecodedLen(len(request.Body)))
+	} else {
+		httpReq.ContentLength = int64(len(request.Body))
 	}
-	url := "https://" + request.RequestContext.DomainName + request.RawPath
-	if request.RawQueryString != "" {
-		url += "?" + request.RawQueryString
-	}
-	ctx = context.WithValue(ctx, ReqCtxKey{}, request)
-	httpRequest, err := http.NewRequestWithContext(ctx, request.RequestContext.HTTP.Method, url, body)
-	if err != nil {
-		return nil, err
-	}
-	httpRequest.RemoteAddr = request.RequestContext.HTTP.SourceIP
-	for k, v := range request.Headers {
-		httpRequest.Header.Add(k, v)
-	}
+	httpReq.Body = io.NopCloser(body)
 
 	respWriter := bufferedHttpRespWriter{header: make(http.Header, 32)}
 	respWriter.body.Grow(8192)
 
-	thisV.HttpHandler.ServeHTTP(&respWriter, httpRequest)
+	thisV.HttpHandler.ServeHTTP(&respWriter, httpReq)
 
-	resp := &events.LambdaFunctionURLResponse{
+	response = &events.LambdaFunctionURLResponse{
 		StatusCode:      respWriter.statusCode,
 		Body:            base64.StdEncoding.EncodeToString(respWriter.body.Bytes()),
-		IsBase64Encoded: true}
+		IsBase64Encoded: true,
+	}
 
 	if hdrCnt := len(respWriter.header); hdrCnt > 0 {
-		resp.Headers = make(map[string]string, hdrCnt)
+		response.Headers = make(map[string]string, hdrCnt)
 		for k, v := range respWriter.header {
 			if k == "Set-Cookie" {
-				resp.Cookies = v
+				response.Cookies = v
 			} else {
-				resp.Headers[k] = strings.Join(v, ",")
+				response.Headers[k] = strings.Join(v, ",")
 			}
 		}
 	}
-	return resp, nil
+	return response, nil
 }
 
 type ReqCtxKey struct{}
+
+var commonHttpReq = http.Request{Close: true}
